7eab3d4c650fdb017405b197aa6023a74447824c
xiaohuiii <18367652239@163.com>
2025-02-26 12:05:52 +0800

    feat: 添加跨域配置

---
+++ src/main/java/com/xiaohui/pocket/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.xiaohui.pocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+/**
+ * CORS 资源共享配置
+ *
+ * @author xiaohui
+ * @since 2025/2/26
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+
+}
